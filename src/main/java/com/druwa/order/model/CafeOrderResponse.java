5f5fd6f400331e9f1e68d9e1112aff2f288b6137
Jongkook <conquerex@gmail.com>
2022-04-30 00:06:47 +0900

    feat: get order List with product list

--- src/main/java/com/druwa/order/model/CafeOrderResponse.java
+++ src/main/java/com/druwa/order/model/CafeOrderResponse.java
@@ -4,29 +4,36 @@ import com.druwa.order.entity.CafeOrder;
 import com.druwa.order.entity.CafeOrderProduct;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CafeOrderResponse {
-    private Long orderId;
+    private Long id;
     private String orderName;
     private String orderDong;
     private String orderHo;
     private LocalDateTime orderDate;
     private String orderState;
-//    private List<CafeOrderProduct> cafeOrderProductList;
+    private List<CafeOrderProductResponse> cafeOrderProductList;
 
     public CafeOrderResponse(CafeOrder cafeOrder) {
-        this.orderId = cafeOrder.getId();
+        this.id = cafeOrder.getId();
         this.orderName = cafeOrder.getOrderName();
         this.orderDong = cafeOrder.getOrderDong();
         this.orderHo = cafeOrder.getOrderHo();
         this.orderDate = cafeOrder.getOrderDate();
         this.orderState = cafeOrder.getOrderState();
-//        this.cafeOrderProductList = cafeOrder.getCafeOrderProductList();
+        this.cafeOrderProductList = getCafeOrderProductList(cafeOrder.getCafeOrderProductList());
     }
 
-    public Long getOrderId() {
-        return orderId;
+    private List<CafeOrderProductResponse> getCafeOrderProductList(List<CafeOrderProduct> list) {
+        List<CafeOrderProductResponse> responseList = new ArrayList<>();
+        list.forEach(cafeOrderProduct -> responseList.add(new CafeOrderProductResponse(cafeOrderProduct)));
+        return responseList;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getOrderName() {
@@ -47,5 +54,9 @@ public class CafeOrderResponse {
 
     public String getOrderState() {
         return orderState;
+    }
+
+    public List<CafeOrderProductResponse> getCafeOrderProductList() {
+        return cafeOrderProductList;
     }
 }
