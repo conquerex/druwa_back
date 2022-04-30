03050bfc20bff993d159eb9be8b3f90b2ed36209
Jongkook <conquerex@gmail.com>
2022-04-30 12:04:27 +0900

    fix: update timezone

--- src/main/java/com/druwa/DruwaApplication.java
+++ src/main/java/com/druwa/DruwaApplication.java
@@ -6,12 +6,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class DruwaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DruwaApplication.class, args);
 		System.out.println("\n\n* * * 드루와~ * * *\n\n");
+	}
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 	@Bean
